2f9a6427fa1136ef15fdca3045a5d3d475f38d5b
ethanlarsen <30578616+ethanlarsen@users.noreply.github.com>
2020-10-19 12:29:17 -0600

    Create AddMeth.java

---
+++ Chapter_4_Introducing_Classes_Objects_and_Methods/AddMeth.java
@@ -0,0 +1,45 @@
+// Add range to Vehicle
+
+/*
+class Vehicle {
+    int passengers; // number of passengers
+    int fuelcap;    // fuel capacity in gallons
+    int mpg;        // fuel consumption in miles per gallon
+
+    // Display the range.
+    void range() {  // The range() method is contained within the Vehicle class.
+        System.out.println("Range is " + fuelcap * mpg); // Notice that fuelcap and mpg are used directly, without the dot operator
+    }
+}
+
+class AddMeth {
+    public static void main(String args[]) {
+        Vehicle minivan = new Vehicle();
+        Vehicle sportscar = new Vehicle();
+
+        /*
+        int range1, range2;
+        */
+
+
+/*
+        // assign values to field in minivan.
+        minivan.passengers = 7;
+        minivan.fuelcap = 16;
+        minivan.mpg = 21;
+
+        // assign values to field in sportscar
+        sportscar.passengers = 2;
+        sportscar.fuelcap = 14;
+        sportscar.mpg = 12;
+
+        System.out.print("Minivan can carry " + minivan.passengers + ". ");
+        
+        minivan.range(); // display range of minivan)
+
+        System.out.print("Sportscar can carry" + sportscar.passengers + ". ");
+
+        sportscar.range(); // display range of sportscar
+    }
+}
+*/
