610b9ad52cdf3d5edc7ee410a23757399bfce47d
ethanlarsen <30578616+ethanlarsen@users.noreply.github.com>
2020-10-19 12:36:52 -0600

    Create AccessDemo.java

---
+++ Chapter_6_A_Closer_Look_At_Methods_and_Classes/AccessDemo.java
@@ -0,0 +1,37 @@
+package Chapter_6_A_Closer_Look_At_Methods_and_Classes;
+
+// Public vs private access.
+class MyClass {
+    private int alpha;  // private access
+    public int beta; // public access
+    int gamma;  //default access
+
+    /*  Methods to access alpha. It is OK for a 
+        member of a class to access a private 
+        member of the same class.
+    */
+    void setAlpha(int a) {
+        alpha = a;
+    }
+    int getAlpha() {
+        return alpha;
+    }
+}
+
+class AccessDemo {
+    public static void main(String args[]) {
+        MyClass ob = new MyClass();
+
+        /*  Access to alpha is allowed only through 
+            its accessor methods.   */
+        ob.setAlpha(-99);
+        System.out.println("ob.alpha is " + ob.getAlpha());
+
+        // You cannot access alpha like this:
+    //  ob.alpha = 10;  // Wront! alpha is private  // <-- Wrong - alpha is private!
+
+        // These are OK because beta and gamma are public.
+        ob.beta = 88;   // <-- OK because these are public
+        ob.gamma = 99;
+    }
+}
