dd1265c8946a8043720ff3032ab45762610ff9e5
ethanlarsen <30578616+ethanlarsen@users.noreply.github.com>
2020-10-19 12:39:43 -0600

    Create BookDemo.java

---
+++ Chapter_8_Packages_and_Interfaces/BookDemo.java
@@ -0,0 +1,36 @@
+// A short package demonstration.
+
+/*
+package Chapter_8_Packages_and_Interfaces.bookpack; // <-- THis file is part of the bookpack package.abstract
+
+class Book {        // <-- Tus, Book is part of backpack
+    private String title;
+    private String author;
+    private int pubDate;
+
+    Book(String t, String a, int d) {
+        title = t;
+        author = a;
+        pubDate = d;
+    }
+
+    void show() {
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(pubDate);
+        System.out.println();
+    }
+}
+
+class BookDemo {    // <-- BookDemo is also part of bookpack
+    public static void main(String args[]) {
+        Book books[] = new Book[5];
+
+        books[0] = new Book("Java: A Beginner's Guide", "Schildt", 2014);
+        books[1] = new Book("Java: The Complete Reference", "Schildt", 2014);
+        books[2] = new Book("The Art of Java", "Schildt and Holmes", 2003);
+        books[3] = new Book("Red Storm Rising", "Clancy", 1986);
+        books[4] = new Book("On the Road", "Kerouac", 1955);
+    }
+}
+*/
