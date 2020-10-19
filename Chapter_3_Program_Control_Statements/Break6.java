307467d9a94c965e656d84643dffb3a4305b7887
ethanlarsen <30578616+ethanlarsen@users.noreply.github.com>
2020-10-19 12:20:28 -0600

    Add files via upload

---
+++ Chapter_3_Program_Control_Statements/Break6.java
@@ -0,0 +1,26 @@
+package Chapter_3_Program_Control_Statements;
+
+// Where you put a label is important
+class Break6 {
+    public static void main(String args[]) {
+        int x=0, y=0;
+
+        // Here, put label before for statement.
+        stop1: for (x=0; x < 5; x++) {
+            for (y=0; x < 5; y++) {
+                if (y==2) break stop1;
+                System.out.println("x and y: "+ x + " " + y);
+            }
+        }
+        
+        System.out.println();
+// Now, put a label immediately before {
+    for(x=0; x < 5; x++)
+    stop2: {
+        for(y=0; y < 5; y++) {
+            if (y==2) break stop2;
+            System.out.println("x and y: " + x + " " + y);
+        }
+    }
+}
+}
