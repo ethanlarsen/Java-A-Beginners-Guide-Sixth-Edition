465499955f6c11eb9818e21376dd2808ac062e64
ethanlarsen <30578616+ethanlarsen@users.noreply.github.com>
2020-10-25 22:23:42 -0600

    Create AppletSkel.java

---
+++ Chapter_15_Applets_Events_and_Miscellaneous_Topics/AppletSkel.java
@@ -0,0 +1,52 @@
+package Chapter_15_Applets_Events_and_Miscellaneous_Topics;
+
+/*
+    Although this skelton does not do anything, it can be compiled
+    and run. Thus, it can be used as a starting point for applets
+    that you create.
+*/
+
+/*
+import java.awt.*;
+import java.applet.*;
+*/
+
+/*
+<applet code="AppletSkel" width=300, height=100>
+</applet>
+*/
+
+
+/*
+public class AppletSkel extends Applet {
+    // Called first.
+    public void init() {
+        //initializatoin
+    }
+*/
+    /* Called second, after init(). Also called whenever
+        the applet is restarted. */
+
+/*   
+    public void start() {
+        // start or resume execution
+    }
+
+    // Called when the applet is stopped.
+    public void stop() {
+        // suspends exevution
+    }
+
+    /* Called when applet is terminated. This is the last
+        method executed. */
+/*
+        public void destroy() {
+            // perform shutdown activities
+        }
+
+        // Called when an AWT-based applet's window must be restored.
+        public void pain(Graphics g) {
+            // redisplay contents of window
+    }
+}
+*/
